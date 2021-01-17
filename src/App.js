10c8258bc9c859f5d88e0d0b6df9c2f752f9dad1
Mdzahin <afsarzahin@gmail.com>
2021-01-17 13:46:54 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import './App.css';
-import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Card from '@material-ui/core/Card';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import WorkIcon from '@material-ui/icons/Work';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import Divider from '@material-ui/core/Divider';
+
+const rows = [
+  {team1:"Zahin",team2:"Naime",date:"01/12/2021",point:"",winner:"Naime"},
+  {team1:"Rinku",team2:"Hamza",date:"01/12/2021",point:"",winner:"Rinku"},
+  {team1:"Naime",team2:"Hamza",date:"01/12/2021",point:"",winner:"Naime"},
+  {team1:"Zahin",team2:"Rinku",date:"01/12/2021",point:"",winner:"Zahin"},
+  {team1:"Zahin",team2:"Hamza",date:"01/12/2021",point:"",winner:"Hamza"},
+  {team1:"Rinku",team2:"Naime",date:"01/12/2021",point:"",winner:"Naime"}
+];
 
 function Schedule() {
-
-  const rows = [
-    {team1:"Zahin",team2:"Naime",date:"01/12/2021",point:"",winner:""},
-  ];
 
   return (
     <TableContainer>
@@ -32,6 +32,7 @@ function Schedule() {
         <TableHead>
           <TableRow>
             <TableCell align="center">Team(1)</TableCell>
+            <TableCell align="center"></TableCell>
             <TableCell align="center">Team(2)</TableCell>
             <TableCell align="center">Date</TableCell>
             <TableCell align="center">Point</TableCell>
@@ -42,6 +43,7 @@ function Schedule() {
           {rows.map((row,i) => (
             <TableRow key={i}>
               <TableCell align="center">{row.team1}</TableCell>
+              <TableCell align="center">VS</TableCell>
               <TableCell align="center">{row.team2}</TableCell>
               <TableCell align="center">{row.date}</TableCell>
               <TableCell align="center">{row.point}</TableCell>
@@ -55,23 +57,58 @@ function Schedule() {
 }
 function Ranking() {
 
-  const ranking = [
-    {name:"Naime",img:"./img/naime.png",point:"Point: 0"},
-    {name:"Rinku",img:"./img/rinku.png",point:"Point: 0"},
-    {name:"Hamza",img:"./img/hamza.png",point:"Point: 0"},
-    {name:"Zahin",img:"./img/zahin.png",point:"Point: 0"},
+  var ranking = [
+    {name:"Zahin",img:"./img/z.png",point:0},
+    {name:"Hamza",img:"./img/h.png",point:0},
+    {name:"Rinku",img:"./img/r.png",point:0},
+    {name:"Naime",img:"./img/n.png",point:0},
   ]
+
+  rows.forEach((a) => {
+    switch (a.winner) {
+      case "Zahin":
+        ranking[0].point = ranking[0].point + 2
+        break;
+      case "Hamza":
+        ranking[1].point = ranking[1].point + 2
+        break;
+      case "Rinku":
+        ranking[2].point = ranking[2].point + 2
+        break;
+      case "Naime":
+        ranking[3].point = ranking[3].point + 2
+        break;
+      default:
+        break;
+    }
+  })
+
+  const sorted = ranking.slice().sort((a, b) => {
+    if (a.point < b.point) {
+        return 1;
+    } else if (a.point > b.point) {
+        return -1;
+    } else {
+        return 0;
+    }
+  });
+
+  ranking = sorted;
+
 
   return (
     <List>
       {ranking.map((team, i) => (
+        <div>
         <ListItem>
         <ListItemAvatar>
           <img style={{marginRight:'30px'}}width="80px" src={team.img} />
         </ListItemAvatar>
-        <ListItemText primary={team.name}/>
-        <ListItemText primary={team.point} />
-      </ListItem>))}
+        <ListItemText primary={`Point:   ${team.point}`} />
+        </ListItem>
+        <Divider />
+        </div>
+        ))}
     </List>
   );
 }
@@ -97,7 +134,7 @@ function App() {
         </Tabs>
         </Paper>
         {
-          value == 0 ? Schedule() : Ranking()
+          value === 0 ? Schedule() : Ranking()
         }
     </div>
   );
